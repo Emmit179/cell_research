104ec7c98df177f0e3d43617b6c032c3185ebb13
emmitph <emmitp@vt.edu>
2023-12-19 15:59:38 -0600

    fixed script

--- nuclear_pore_membrane.py
+++ nuclear_pore_membrane.py
@@ -1,74 +1,96 @@
 import numpy as np
 from skimage import io
 from skimage.measure import label, regionprops
-import sys
+from scipy.ndimage import rotate, binary_closing
 
 # load file with skimage
 ground_truth = io.imread('data/00_Cell1_2_Crop1_Back3_steve_ground_truth.tif')
 
-#function for rotating coords around centroid
-def rotate(coords, centroid, angle):
-    angle_rad = np.radians(angle)
+ground_truth_flipped = np.swapaxes(ground_truth, 0, 2)
 
-    translated_coords = coords - centroid
+# closed_ground_truth = binary_closing(ground_truth_flipped)
 
-    rotation_matrix = np.array([[np.cos(angle_rad), -np.sin(angle_rad), 0],
-                                [np.sin(angle_rad), np.cos(angle_rad), 0],
-                                [0, 0, 1]])
+labeled_volume = label(ground_truth_flipped)
 
-    rotated_coords = np.dot(translated_coords, rotation_matrix.T)
+pores = regionprops(labeled_volume)
 
-    rotated_coords += centroid
+def vertical(coords, centroid):
+    cur = round(centroid[1])
 
-    return rotated_coords
+    radius = False
 
-# divide into pores and use regionprops for centroid/labels
-labeled_pores = label(ground_truth)
+    while coords[round(centroid[0])][cur][round(centroid[2])] == 0 and coords[round(centroid[0])][cur][round(centroid[2]) - 1] == 0 and coords[round(centroid[0])][cur][round(centroid[2]) + 1] == 0:
+        cur += 1
 
-pore_props = regionprops(labeled_pores)
+        if (len(coords[round(centroid[0])]) <= cur):
+            radius = True
+            break
 
-for pore in pore_props:
-    coords = np.array(pore.coords)
+    other = round(centroid[1])
+
+    while coords[round(centroid[0])][other][round(centroid[2])] == 0 and coords[round(centroid[0])][other][round(centroid[2]) - 1] == 0 and coords[round(centroid[0])][other][round(centroid[2]) + 1] == 0:
+        other -= 1
+
+        if (other < 0):
+            return abs(cur - centroid[1]) * 2
+
+    if radius:
+        return abs(other - centroid[1]) * 2
     
-    angles = np.arange(0, 180, 10)
+    return abs(cur - other)
+
+def horizontal(coords, centroid):
+    cur = round(centroid[0])
+
+    radius = False
+
+    while coords[cur][round(centroid[1])][round(centroid[2])] == 0 and coords[cur][round(centroid[1])][round(centroid[2]) - 1] == 0 and coords[cur][round(centroid[1])][round(centroid[2]) + 1] == 0:
+        cur += 1
+
+        if (len(coords) <= cur):
+            radius = True
+            break
+
+    other = round(centroid[0])
+
+    while coords[other][round(centroid[1])][round(centroid[2])] == 0 and coords[other][round(centroid[1])][round(centroid[2]) - 1] == 0 and coords[other][round(centroid[1])][round(centroid[2]) + 1] == 0:
+        other -= 1
+
+        if (other < 0):
+            return abs(cur - centroid[0]) * 2
+
+    if radius:
+        return abs(other - centroid[0]) * 2
+
+    return abs(cur - other)
+    
+
+i = 1
+
+threshold = 150
+
+for pore in pores:
+    if len(pore.coords) < threshold:
+        continue
+    # print(len(pore.coords))
+    angles = np.arange(0, 90, 18)
+
     diameters = []
 
     # for each angle 10 deg 18 times
     for angle in angles:
-        centroid = pore.centroid
+        # Rotate the region by a specific angle (e.g., 10 degrees)
+        rotated_region = rotate(labeled_volume == pore.label, angle=angle, axes=(0, 1), order=0, mode='nearest').astype(int)
+        temp = regionprops(rotated_region)
 
-        rotated_coords = np.array(rotate(coords, centroid, angle))
+        centroid = temp[0].centroid
         
-        # create x axis at angle then plug into original image for intensities along axis
-        x_intensity = []
-        for x_cord in rotated_coords[:, 0]:
-            y_cord = centroid[1]
-            z_cord = centroid[2]
-            # print([int(x_cord), int(y_cord), int(z_cord)])
-            x_intensity.append(ground_truth[int(x_cord), int(y_cord), int(z_cord)])
-        
-        #repeat for y axis
-        y_intensity = []
-        for y_cord in rotated_coords[:, 1]:
-            x_cord = centroid[0]
-            z_cord = centroid[2]
-            y_intensity.append(ground_truth[int(x_cord), int(y_cord), int(z_cord)])
-
-        # get maxima indices along axes (i think instead of min because alphas are inverse to example)
-        x_maxima_indices = np.where(x_intensity == np.max(x_intensity))[0]
-        y_maxima_indices = np.where(y_intensity == np.max(y_intensity))[0]
-
-        # exit()
-
-        # get difference of pixels and subtract one to get number of pixels between both maxes
-        x_maxima_distance = np.abs(x_maxima_indices[1] - x_maxima_indices[0] - 1)
-        y_maxima_distance = np.abs(y_maxima_indices[1] - y_maxima_indices[0] - 1)
-
-        # append both to diameter
-        diameters.append(x_maxima_distance)
-        diameters.append(y_maxima_distance)
+        diameters.append(vertical(rotated_region, centroid))
+        diameters.append(horizontal(rotated_region, centroid))
 
     # get mean diameter of all angles
     average_diameter = np.mean(diameters)
 
-    print(f"Pore Number {pore.label} Diameter: {average_diameter}")
+    print(f"Pore Centroid: {pore.centroid} Diameter: {average_diameter}")
+
+    i += 1
